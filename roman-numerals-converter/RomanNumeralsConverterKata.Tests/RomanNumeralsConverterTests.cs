3124afd326b5f8563f348871304136a775b25d08
Josh Egan <josh.egan145@gmail.com>
2015-05-20 14:57:16 -0600

    chore(*): create projects and prepare environment

---
+++ roman-numerals-converter/RomanNumeralsConverterKata.Tests/RomanNumeralsConverterTests.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+
+namespace RomanNumeralsConverterKata.Tests
+{
+    [TestFixture]
+    public class RomanNumeralsConverterTests
+    {
+        [Test]
+        public void can_convert_arabic_to_roman()
+        {
+            var converter = new RomanNumeralsConverter();
+
+            Assert.That(converter.ToRoman(1), Is.EqualTo("I"));
+        }
+    }
+}
---
+++ roman-numerals-converter/RomanNumeralsConverterKata/RomanNumeralsConverter.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RomanNumeralsConverterKata
+{
+    public class RomanNumeralsConverter
+    {
+        public string ToRoman(int arabic)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
