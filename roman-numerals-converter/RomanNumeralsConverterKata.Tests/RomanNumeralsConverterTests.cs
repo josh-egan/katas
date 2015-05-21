8c78e534b77b99d5566d9a2be6e238be6acea9c4
Josh Egan <josh.egan145@gmail.com>
2015-05-21 15:13:18 -0600

    Implement arabic to roman up to tens

--- roman-numerals-converter/RomanNumeralsConverterKata.Tests/RomanNumeralsConverterTests.cs
+++ roman-numerals-converter/RomanNumeralsConverterKata.Tests/RomanNumeralsConverterTests.cs
@@ -5,12 +5,27 @@ namespace RomanNumeralsConverterKata.Tests
     [TestFixture]
     public class RomanNumeralsConverterTests
     {
-        [Test]
-        public void can_convert_arabic_to_roman()
-        {
-            var converter = new RomanNumeralsConverter();
+        private RomanNumeralsConverter _converter;
 
-            Assert.That(converter.ToRoman(1), Is.EqualTo("I"));
+        [SetUp]
+        public void Setup()
+        {
+            _converter = new RomanNumeralsConverter();
+        }
+
+        [TestCase(1,"I")]
+        [TestCase(2,"II")]
+        [TestCase(3,"III")]
+        [TestCase(4,"IV")]
+        [TestCase(5,"V")]
+        [TestCase(7,"VII")]
+        [TestCase(9,"IX")]
+        [TestCase(10,"X")]
+        [TestCase(14,"XIV")]
+        [TestCase(15,"XV")]
+        public void can_convert_arabic_to_roman(int arabic, string expectedRoman)
+        {
+            Assert.That(_converter.ToRoman(arabic), Is.EqualTo(expectedRoman));
         }
     }
 }
--- roman-numerals-converter/RomanNumeralsConverterKata/RomanNumeralsConverter.cs
+++ roman-numerals-converter/RomanNumeralsConverterKata/RomanNumeralsConverter.cs
@@ -1,12 +1,31 @@
-﻿using System;
+﻿using System.Collections.Generic;
 
 namespace RomanNumeralsConverterKata
 {
     public class RomanNumeralsConverter
     {
+        private static readonly Dictionary<int, string> Map = new Dictionary<int, string>
+        {
+            {10, "X"},
+            {9, "IX"},
+            {5, "V"},
+            {4, "IV"},
+            {1, "I"}
+        }; 
+
         public string ToRoman(int arabic)
         {
-            throw new NotImplementedException();
+            var result = "";
+            foreach (var p in Map)
+            {
+                while (p.Key <= arabic)
+                {
+                    result += p.Value;
+                    arabic -= p.Key;
+                }
+            }
+
+            return result;
         }
     }
 }
