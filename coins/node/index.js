81f5a4c3ccd3002a7a2d3d182dc91bba23646fbe
Josh Egan <josh.egan145@gmail.com>
2017-02-21 19:48:54 -0700

    refactor makeChange method

--- coins/node/index.js
+++ coins/node/index.js
@@ -4,32 +4,38 @@ module.exports = {
   makeChange(money, coins) {
     if (!coins || !Array.isArray(coins))
       throw new Error('An array of coins must be provided.')
-
     if (!money)
       return []
 
-    let leastCoinsSolution
-    const orderedCoins = coins.sort((curr, prev) => prev - curr)
-
-    for (let startingIndex = 0; startingIndex < orderedCoins.length; startingIndex++){
-      const change = []
-      let remainingMoney = money
-      for (let i = startingIndex; i < orderedCoins.length; i++) {
-        if(leastCoinsSolution && change.length > leastCoinsSolution.length)
-          break;
-        let coin = orderedCoins[i]
-        while (remainingMoney / coin >= 1) {
-          change.push(coin)
-          if(leastCoinsSolution && change.length > leastCoinsSolution.length)
-            break;
-          remainingMoney = parseFloat((remainingMoney - coin).toFixed(6))
-        }
-      }
-
-      if (!leastCoinsSolution || change.length < leastCoinsSolution.length)
-        leastCoinsSolution = change
-    }
-
-    return leastCoinsSolution
+    const orderedCoins = sortLargestToSmallest(coins)
+    return getLeastCoins(money, orderedCoins)
   }
+}
+
+function sortLargestToSmallest(coins) {
+  return coins.sort((curr, prev) => prev - curr)
+}
+
+function getLeastCoins(money, orderedCoins) {
+  const coinSets = orderedCoins.map((_, i, arr) => arr.slice(i))
+
+  return coinSets.reduce((least, coinSet) => {
+    let remainingMoney = money
+    const change = []
+
+    coinSet.forEach(coin => {
+      while (remainingMoney / coin >= 1) {
+        change.push(coin)
+        remainingMoney = parseFloat((remainingMoney - coin).toFixed(6))
+        if (!isFewerCoins(least, change))
+          break;
+      }
+    })
+
+    return isFewerCoins(least, change) ? change : least
+  }, null)
+}
+
+function isFewerCoins(prevCoins, newCoins) {
+  return !prevCoins || newCoins.length < prevCoins.length
 }
